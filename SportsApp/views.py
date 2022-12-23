34ed00c341473f51399bfb79b26994ef3cf83a0d
JFrieden <jfrieden19@gmail.com>
2022-12-23 04:23:13 -0600

    Overview of Django Routing

--- SportsApp/views.py
+++ SportsApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, me, you've reached the index of your app")
+
+def test(request):
+    return HttpResponse("Sup b")
